eca0fefc0137ad3406675302162d0d3f6a29604c
Liadenia <121140535+Liadenia@users.noreply.github.com>
2023-01-26 15:40:33 +0900

    Gallery site update
    change files

--- js/sub_script.js
+++ js/sub_script.js
@@ -1,8 +1,4 @@
 $(function(){
-// -------------------------------loading page----------------------------
-    // $(window).container(function() { $(".container").hide();});
-
-
 /*---------------------------Gnb desk-menu------------------------- */
     let mouseoverstate=false;
     $('.sub').hide();
@@ -51,55 +47,7 @@ $(function(){
         $('.bg_menu').fadeToggle(400)
     });
 /*----------------------------Toggle menu finish------------------ */
-/*---------------------------flip-card---------------------------- */
-    
-    
-    for(let i=0; i<3; i++ ){
-        document.querySelectorAll('.card')[i].addEventListener('click',function(event){
-            let elem=event.currentTarget;
-        if (elem.style.transform == "rotateY(180deg)") {
-                elem.style.transform = "rotateY(0deg)";
-            } else {
-                elem.style.transform = "rotateY(180deg)";
-            }
-        })
-    }
 
-    // let card = document.querySelector('.card');
-    
-    // card.addEventListener('click', click);
-    // function click(event) {
-    // let elem=event.currentTarget;
-    //     if (elem.style.transform == "rotateY(180deg)") {
-    //             elem.style.transform = "rotateY(0deg)";
-    //         } else {
-    //             elem.style.transform = "rotateY(180deg)";
-    //         }
-    // }
-    
-    // $(function(){
-    //     $('.front').quickFlip('.back');
-    // });
-
-
-
-/*---------------------------flip-card finish--------------------- */
-/*--------------------------Gallery Sub 연결----------------------*/
-/*
-    $('h1').on('click',function(){
-        window.location.href="../gallery_index.html";
-    });
-    $('.sub1-1').on('click',function(){
-        window.location.href="../Gallery/gallery_intro.html";
-    });
-    $('.sub2-1').on('click',function(){
-        window.location.href="../Art_space/gallery_space.html";
-    });
-    $('.sub4-1').on('click',function(){
-        window.location.href="../Artist/gallery_artist.html";
-    });
-*/
-/*--------------------------Gallery Sub 끝----------------------*/
 });
 
 
